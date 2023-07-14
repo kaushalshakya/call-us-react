270fcb9a0c83b62801ba51d62e736b2783152f40
Kaushal Shakya <kshakya101@gmail.com>
2023-07-14 15:07:16 +0545

    Sidebar design with dropdown done

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
-import Sidebar from '../components/sidebar'
+import { Link } from 'react-router-dom';
+import SidebarElements from '../components/SidebarElements';
+import dashboardImg from '../assets/dashboard.png';
+import userImg from '../assets/user.png';
+import entityImg from '../assets/file.png';
+import inboundImg from '../assets/inbound.png';
+import outBoundImg from '../assets/outbound.png';
+import reminderImg from '../assets/bell.png';
+import ticketImg from '../assets/ticket.png';
 
-export default function DashboardLayout({children}) {
+
+export default function Sidebar({children}) {
   return (
     <>
-       <Sidebar/>
-
-
-        <div>{children}</div>
+      <div className="bg-orange-400  text-black h-screen">
+          <div className="bg-white w-[230px] h-screen pl-[10px gap-8">
+              <ul className=" flex flex-col px-4 gap-3">
+                <h1 className='font-bold text-base'>General</h1>
+                    <Link to='/'><SidebarElements icon = {dashboardImg} buttonName = 'Dashboard'/></Link>  
+                    <Link to='/'><SidebarElements icon = {userImg} buttonName = 'Manage Users' subList={[{item: "Users"}, {item: "Branches"}, {item: "Departments"}]}  /></Link> 
+                    <Link to='/'><SidebarElements icon = {entityImg} buttonName = 'Entity' subList={[{item: "Users"}, {item: "Branches"}, {item: "Departments"}]} /></Link> 
+                    <Link to='/'><SidebarElements icon = {inboundImg} buttonName = 'Inbound Calls' subList={[{item: "Users"}, {item: "Branches"}, {item: "Departments"}]} /></Link> 
+                    <Link to='/'><SidebarElements icon = {outBoundImg} buttonName = 'Outbound Calls' subList={[{item: "Users"}, {item: "Branches"}, {item: "Departments"}]} /></Link> 
+                    <Link to='/'><SidebarElements icon = {reminderImg} buttonName = 'Reminder Calls' subList={[{item: "Users"}, {item: "Branches"}, {item: "Departments"}]} /></Link> 
+                    <Link to='/'><SidebarElements icon = {ticketImg} buttonName = 'Tickets' subList={[{item: "Users"}, {item: "Branches"}, {item: "Departments"}]} /></Link> 
+              </ul>
+          </div>
+          <div>
+            {children}
+          </div>
+      </div>
     </>
   )
 }
--- src/components/SidebarElements.jsx
+++ src/components/SidebarElements.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function SidebarElements({icon, elementName}) {
+export default function SidebarElements({icon, buttonName, subList}) {
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleDropdownClick = () =>{
+    setDropdown((value) => !value );  
+  }
+  console.log(subList);
+  //const list = subList.map((data) => data.item);
+  //console.log(list);
+
   return (
     <>
-        <li className='flex gap-2'>
-            <img src= {icon} className='h-[35px]' />{elementName}
-        </li>
+      <li className='w-full'>
+        <div className='w-full text-black hover:text-white hover:bg-orange-300 rounded p-3'>
+        <div className='flex flex-row gap-2 text-sm justify-between items-center' onClick={handleDropdownClick}>
+          <div className='flex flex-row gap-2 font-medium'>
+          <img src= {icon} className='h-[25px]' /> {buttonName}
+          </div>
+          {subList && <svg className="fill-current h-4 w-4 ml-2 relative left 100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 20">
+          <path d="M9 9l-3 3-3-3" fill="none" stroke="currentColor" stroke-width="2"/></svg>}
+          
+        </div>
+        </div>
+        <ul className= {dropdown ? 'flex flex-col bg-white pt-1 shadow-lg' : 'hidden'}>
+        {subList && subList.map((el, index) => (
+          <li key={index}>{el.item}</li>
+        ))}
+        </ul>     
+      </li>  
     </>
   )
 }
